5ae8c2fbd4db2fa5606426228f841e97ba24b6a6
Cayque Silva <cayque.ti@gmail.com>
2024-04-30 16:36:58 -0300

    feat: add initial search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Search from "./_components/search";
 import Header from "./_components/ui/header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
