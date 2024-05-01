ea659c1824c047ce26435adca92610fa1278e1fa
Cayque Silva <cayque.ti@gmail.com>
2024-05-01 17:28:45 -0300

    feat: add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Search from "./_components/search";
 import Header from "./_components/header";
 import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,6 +13,18 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
