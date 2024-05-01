afd774afacba220fc55e48b4fe8a5224f7e4bf6a
Cayque Silva <cayque.ti@gmail.com>
2024-05-01 19:33:10 -0300

    feat: add restaurant list and items component

---
+++ app/_components/restaurant-list.tsx
@@ -0,0 +1,20 @@
+import { db } from "../_lib/prisma";
+import RestaurantItem from "./restaurant-item";
+
+const RestaurantList = async () => {
+  //PEGAR RESTAURANTES COM MAIOR NUMERO DE PEDIDOS...
+  const restaurants = await db.restaurant.findMany({
+    take: 10,
+  });
+  return (
+    <>
+      <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+        {restaurants.map((restaurant) => (
+          <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RestaurantList;
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   //pega categoria
@@ -56,6 +57,20 @@ const Home = async () => {
 
       <div className="px-5 pt-6">
         <PromoBanner src="/promo-banner-02.png" alt="Até partir de R$17,90" />
+      </div>
+
+      <div className="space-y-3 py-5 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
