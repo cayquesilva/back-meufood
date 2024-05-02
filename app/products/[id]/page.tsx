404e8af90bfbd33def125bc811d97356d8ca2e4c
Cayque Silva <cayque.ti@gmail.com>
2024-05-02 12:05:16 -0300

    fix: ensure juices for the same restaurant

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -28,6 +28,9 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
       category: {
         name: "Sucos",
       },
+      restaurant: {
+        id: product?.restaurant.id,
+      },
     },
     include: {
       restaurant: true,
