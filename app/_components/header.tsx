621178d7689f726442c25706558147b8bac05b36
Cayque Silva <cayque.ti@gmail.com>
2024-05-03 00:43:57 -0300

    feat: add link para homepage na img header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="MeuFood" height={30} width={100} />
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="MeuFood"
+            fill
+            className="object-contain"
+          />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
