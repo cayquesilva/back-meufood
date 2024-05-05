68fd867c070475fce95951d92d724a521a290442
Cayque Silva <cayque.ti@gmail.com>
2024-05-05 17:04:58 -0300

    fix: ignores discount for delivery

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -66,12 +66,15 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   }, [products]);
 
   const totalPrice = useMemo(() => {
-    return products.reduce((acc, product) => {
-      return acc + calculateProductTotalPrice(product) * product.quantity;
-    }, 0);
+    return (
+      products.reduce((acc, product) => {
+        return acc + calculateProductTotalPrice(product) * product.quantity;
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
+    );
   }, [products]);
 
-  const totalDiscounts = subTotalPrice - totalPrice;
+  const totalDiscounts =
+    subTotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const removeProductFromCart = (productId: string) => {
     return setProducts((prev) =>
