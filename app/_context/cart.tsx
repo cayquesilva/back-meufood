00e37cfad23d720cffb1878109a57d5c33215def
Cayque Silva <cayque.ti@gmail.com>
2024-05-05 17:47:00 -0300

    feat: add card-banner component

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -23,6 +23,7 @@ interface ICartContext {
   subTotalPrice: number;
   totalPrice: number;
   totalDiscounts: number;
+  totalQuantity: number;
   addProductToCart: ({
     product,
     quantity,
@@ -50,6 +51,7 @@ export const CartContext = createContext<ICartContext>({
   subTotalPrice: 0,
   totalPrice: 0,
   totalDiscounts: 0,
+  totalQuantity: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -71,6 +73,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
       }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
     );
+  }, [products]);
+
+  const totalQuantity = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
   }, [products]);
 
   const totalDiscounts =
@@ -171,6 +179,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         subTotalPrice,
         totalPrice,
         totalDiscounts,
+        totalQuantity,
       }}
     >
       {children}
